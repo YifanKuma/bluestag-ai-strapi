759c7d47a181b0d83d6f8aa8755285ecab5c504e
YifanKuma <yifan.huang0219@gmail.com>
2025-12-02 15:15:54 +1000

    Add production CORS + CSP for Vercel

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,56 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    'strapi::logger',
+    'strapi::errors',
+    {
+        name: 'strapi::security',
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    "default-src": ["'self'"],
+                    "connect-src": [
+                        "'self'",
+                        "https:",
+                        "http:",
+                        "https://bluestag-frontend.vercel.app"
+                    ],
+                    "img-src": [
+                        "'self'",
+                        "data:",
+                        "blob:",
+                        "https:",
+                        "http:",
+                        "https://bluestag-ai-strapi.onrender.com"
+                    ],
+                    "media-src": [
+                        "'self'",
+                        "data:",
+                        "blob:",
+                        "https:",
+                        "http:",
+                        "https://bluestag-ai-strapi.onrender.com"
+                    ],
+                    upgradeInsecureRequests: null,
+                },
+            },
+        },
+    },
+    {
+        name: 'strapi::cors',
+        config: {
+            enabled: true,
+            headers: '*',
+            origin: [
+                "http://localhost:3000",
+                "https://bluestag-frontend.vercel.app",
+                "https://bluestag-ai-strapi.onrender.com"
+            ],
+        },
+    },
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
 ];
